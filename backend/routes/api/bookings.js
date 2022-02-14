e1f971cff4a151f6770386bc923874a5592830b3
davigravi <davidlee777@gmail.com>
2022-02-14 08:36:25 -0600

    last one

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,20 +7,21 @@ const router = express.Router();
 
 router.get('/:userId', asyncHandler(async function (req, res) {
 
-    if(req.params.userId > 3){
 
-        const bookings = await db.Booking.findAll({where: {userId: req.params.userId}});
-        console.log(bookings, "bookings")
-        if(bookings.lenght === 0){
-            return res.json(bookings)
-        }else {
-            const bookingsDemo = await db.Booking.findAll();
-            return res.json(bookingsDemo)
-        }
-    }
-    const bookings1 = await db.Booking.findAll({where: {userId: req.params.userId}});
+    // if(req.params.userId > 3){
 
-    return res.json(bookings1);
+    //     const bookings = await db.Booking.findAll({where: {userId: req.params.userId}});
+    //     console.log(bookings, "bookings")
+    //     if(bookings.length === 0){
+    //         return res.json(bookings)
+    //     }else {
+    //         const bookingsDemo = await db.Booking.findAll();
+    //         return res.json(bookingsDemo)
+    //     }
+    // }
+    const bookings = await db.Booking.findAll({where: {userId: req.params.userId}});
+
+    return res.json(bookings);
 })
 );
 
